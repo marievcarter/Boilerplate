3534c3f39862eacbb3611217f8a7dee4739c4bc1
Marie Carter <marievcarter@gmail.com>
2019-01-06 20:48:56 -0500

    Add auth folder for local login and eventual oauth2. Also added route to get all users.

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 // all sample-related routes here
-router.use('/sample', require('./sample.js'));
+router.use('/users', require('./users.js'));
 
 // 404 handle all other requests
 router.use((req, res, next) => {
